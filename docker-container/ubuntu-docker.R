f1f1eb090e429dd0374ee1afb3eecee48c0861a8
dlemas <djlemas@ufl.edu>
2018-09-18 08:54:51 -0400

    ubuntu docker

---
+++ docker-container/ubuntu-docker.R
@@ -0,0 +1,10 @@
+# asciinema Docker Images (https://hub.docker.com/r/asciinema/asciinema/)
+# windows
+
+# 1) open docker teminal
+
+# 2) pull container
+docker pull asciinema/asciinema
+
+# 3a) boot into container
+docker run -ti asciinema/asciinema bash
